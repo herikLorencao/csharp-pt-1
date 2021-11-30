83fe4ed6a145a3768d7a9bf3c1444cc70fbbc073
Herik S. Lorenção <herik.lorencao@itixti.com.br>
2021-11-30 11:14:15 -0300

    adicionando variáveis.

---
+++ 2-CriandoVariaveis/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_CriandoVariaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var idade = 10;
+            Console.WriteLine(idade);
+
+            int idade2 = 30;
+            Console.WriteLine(idade2);
+
+            Console.WriteLine("Tecle enter para sair ...");
+            Console.ReadLine();
+        }
+    }
+}
