75161f65286b808f3935d342ac33f6a56fad0d2b
Herik S. Lorenção <herik.lorencao@itixti.com.br>
2021-12-14 11:06:34 -0300

    adicionando conversões e tipos numéricos

---
+++ 4-ConversoesEOutrosTipos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _4_ConversoesEOutrosTipos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 4");
+
+            double salario = 1200.50;
+            // int possui 32 bits
+            int salarioEmInteiro = (int)salario;
+            Console.WriteLine(salarioEmInteiro);
+
+            // long possui 64 bits
+            long idade = 130000000000000;
+            Console.WriteLine(idade);
+
+            // o short possui 16 bits
+            short quantidadeProdutos = 15000;
+            Console.WriteLine(quantidadeProdutos);
+
+            // pouco utilizado por padrão suporta 32 bits
+            float altura = 1.80f;
+            Console.WriteLine(altura);
+
+            Console.ReadLine();
+        }
+    }
+}
