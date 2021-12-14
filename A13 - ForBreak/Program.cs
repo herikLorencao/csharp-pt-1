5a445d8db8f970499d78b5d56b5e247991342bdf
Herik S. Lorenção <herik.lorencao@itixti.com.br>
2021-12-14 14:23:42 -0300

    adicionando for com break

---
+++ A13 - ForBreak/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace A13___ForBreak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 13");
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j <= 10; j++)
+                {
+                    Console.Write('*');
+                    if (j >= i)
+                        break;
+                }
+                Console.WriteLine();
+            }
+
+            // Forma sem break
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
