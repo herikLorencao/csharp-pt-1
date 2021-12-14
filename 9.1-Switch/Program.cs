046d9d74c434aee72dc390b45fdc7f3530db68db
Herik S. Lorenção <herik.lorencao@itixti.com.br>
2021-12-14 13:31:10 -0300

    adicionando switch

---
+++ 9.1-Switch/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _9._1_Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idadeJoao = 20;
+            
+            switch (idadeJoao)
+            {
+                case 10:
+                    // nas versões novas dá para fazer condições como o exemplo abaixo:
+                    // case < 10:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
