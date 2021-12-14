c17de8af00e9fa4fe458a6ca5b103dbe68883b5b
Herik S. Lorenção <herik.lorencao@itixti.com.br>
2021-12-14 13:57:37 -0300

    adicionando for

---
+++ A10 - While/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace A10___While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 10");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            while (contadorMes <= 12)
+            {
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Valor para o mês " + contadorMes + " é igual a R$ " + valorInvestido);
+                contadorMes++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ A11 - For/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A11___For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 11");
+
+            double valorInvestido = 1000;
+
+            for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+            {
+                valorInvestido *= (1 + 0.0036);
+                Console.WriteLine("Valor para o mês " + contadorMes + " é igual a R$ " + valorInvestido);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
