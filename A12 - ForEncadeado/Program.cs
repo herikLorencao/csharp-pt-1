4bb726aa2727fc8f7e78c3de831a8b44470f3cd1
Herik S. Lorenção <herik.lorencao@itixti.com.br>
2021-12-14 14:09:40 -0300

    adicionando for encadeado

---
+++ A12 - ForEncadeado/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace A12___ForEncadeado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 12");
+
+            double valorInvestido = 1000;
+            double fatorInvestimento = 1 + 0.0036;
+
+            for (int ano = 0; ano < 5; ano++)
+            {
+                for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+                {
+                    valorInvestido *= fatorInvestimento;
+                }
+
+                fatorInvestimento += 0.0010;
+            }
+
+            Console.WriteLine("O valor total ao final do investimento será de R$ " + valorInvestido);
+            Console.ReadLine();
+        }
+    }
+}
